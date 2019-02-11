54333becdfb3c1619a551da87420c3cb1a0292e3
nishant1307 <nishant.mitra94@gmail.com>
2019-02-11 10:18:52 +0530

    Thing image rectify

--- kramaaClient/ThingPage.js
+++ kramaaClient/ThingPage.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import { BrowserRouter, Route, Link} from "react-router-dom";
 import axios from "axios";
-import { Col, Button, Card, CardHeader, CardBody, ListGroupItem, ListGroup } from 'reactstrap';
+import { Row, Col, Button, Card, CardHeader, CardBody, ListGroupItem, ListGroup, Input } from 'reactstrap';
 
 class ThingPage extends Component {
   constructor(props) {
@@ -13,10 +13,19 @@ class ThingPage extends Component {
       brand: '',
       associationStatus: '',
       uri: [],
+      deviceList: [],
+      selectedDevice: ''
     };
     this.componentDidMount = this.componentDidMount.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.fetchDeviceList = this.fetchDeviceList.bind(this);
   }
 
+  handleChange(e) {
+    const { name, value } = e.target;
+    console.log("name", name, "value", value);
+    this.setState({ [name]: value });
+  }
 
   componentDidMount() {
     axios.post('/api/things/getThingInfo', {'thingID': this.props.match.params.thingID, clientToken: sessionStorage.getItem("clientToken")}).then(res => {
@@ -34,33 +43,61 @@ class ThingPage extends Component {
       }
     })
   }
+
+  fetchDeviceList(){
+    axios.post("/api/devices/deviceList", {clientToken: sessionStorage.getItem("clientToken")})
+    .then(res=> {
+      this.setState({
+        deviceList: res.data.deviceList
+      })
+      this.forceUpdate();
+    });
+  }
   render(){
-    const {name, description, id, brand, associationStatus, uri} = this.state;
-    console.log(uri);
-    console.log(uri.length);
+    const {name, description, id, brand, associationStatus, uri, deviceList, selectedDevice} = this.state;
     let imageRender = [];
     for(let i=0; i<uri.length; i++){
-      imageRender.push(<ListGroupItem>Image {i+1}: <img src={"https://gateway.ipfs.io/ipfs/"+uri[i]} /></ListGroupItem>);
+      imageRender.push(<ListGroupItem>Image {i+1}: <img width="300" height="300" src={"https://gateway.ipfs.io/ipfs/"+uri[i]} /></ListGroupItem>);
+    }
+
+    let dropdownRender;
+
+    if(deviceList.length>0){
+      dropdownRender = [<option key= "" name= "" value="">Select Device URN</option>];
+      let j;
+      for(var i=0;i<deviceList.length; i++){
+        j= deviceList[i].uniqueId;
+        k= deviceList[i].urn
+        dropdownRender.push(<option key= {j} name= {j} value={j}>{k}</option>);
+      }
     }
     return(
       <div>
         <Link to="/things"><Button color="primary">Back to Thing List</Button></Link><br /><br/>
-        <Col sm="12" xl="6">
-          <Card>
-            <CardHeader>
-              <i className="fa fa-align-justify"></i><strong>Thing Name: {name}</strong>
-            </CardHeader>
-            <CardBody>
-              <ListGroup>
-                <ListGroupItem>Thing ID: {id}</ListGroupItem>
-                <ListGroupItem>Description: {description}</ListGroupItem>
-                <ListGroupItem>Brand: {brand}</ListGroupItem>
-                {imageRender}
-                <ListGroupItem>Association to any device?: {String(associationStatus)}</ListGroupItem>
-              </ListGroup>
-            </CardBody>
-          </Card>
-        </Col>
+        <Row>
+          <Col xs="12" sm="6">
+            <Card>
+              <CardHeader>
+                <i className="fa fa-align-justify"></i><strong>Thing Name: {name}</strong>
+              </CardHeader>
+              <CardBody>
+                <ListGroup>
+                  <ListGroupItem>Thing ID: {id}</ListGroupItem>
+                  <ListGroupItem>Description: {description}</ListGroupItem>
+                  <ListGroupItem>Brand: {brand}</ListGroupItem>
+                  {imageRender}
+                  <ListGroupItem>Association to any device?: {String(associationStatus)}</ListGroupItem>
+                </ListGroup>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col xs="12" sm="6">
+            <Button color="primary" onClick={this.fetchDeviceList}>Assign to device </Button>
+            <Input type="select" name= "selectedDevice" value= {selectedDevice} onChange={this.handleChange} id="select">
+              {dropdownRender}
+            </Input>
+          </Col>
+        </Row>
       </div>
     )
   }
