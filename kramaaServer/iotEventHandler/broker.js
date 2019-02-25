f41c0272c1dc142d8f1fa68ad761693e43e94800
nishant1307 <nishant.mitra94@gmail.com>
2019-02-25 16:58:08 +0530

    Broker

--- kramaaServer/iotEventHandler/broker.js
+++ kramaaServer/iotEventHandler/broker.js
@@ -9,3 +9,7 @@ var server = new mosca.Server(settings);
 server.on('ready', function(){
   console.log("ready");
 });
+
+server.on('clientConnected', function(client) {
+    console.log('client connected', client.id);
+});
