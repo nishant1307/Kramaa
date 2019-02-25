b3ee3072e90b932f1f8714178b7dc739c9709461
nishant1307 <nishant.mitra94@gmail.com>
2019-02-25 12:26:48 +0530

    Hf

--- kramaaClient/store.js
+++ kramaaClient/store.js
@@ -3,7 +3,6 @@ import { persistStore, persistReducer } from 'redux-persist'
 import storageSession from 'redux-persist/lib/storage/session'
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
-
 const inititalState = {};
 
 const persistConfig = {
